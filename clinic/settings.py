f875ba55f88f6d1d97d72212f0b01a86c199eb15
m-a-99 <mohammedayyad12022@gmail.com>
2025-01-09 14:43:47 +0200

    4rd commit

--- clinic/settings.py
+++ clinic/settings.py
@@ -70,9 +70,11 @@ CORS_ALLOW_ALL_ORIGINS = True  # If this is used then `CORS_ALLOWED_ORIGINS` wil
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
+    'https://clinicapp-f9w4.onrender.com',
 ]  # If this is used, then not need to use `CORS_ALLOW_ALL_ORIGINS = True`
 CORS_ALLOWED_ORIGIN_REGEXES = [
     'http://localhost:3000',
+    'https://clinicapp-f9w4.onrender.com',
 ]
 
 ROOT_URLCONF = 'clinic.urls'
