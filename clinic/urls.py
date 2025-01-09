b7ce68347ecfa97fae08daafb17bf47aaee3389d
m-a-99 <mohammedayyad12022@gmail.com>
2025-01-09 14:32:59 +0200

    3rd commit

--- clinic/urls.py
+++ clinic/urls.py
@@ -3,12 +3,11 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns =static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+ [
+urlpatterns =[
     path('admin/', admin.site.urls),
     path('backend/', include("user_auth.urls")),
     path('backend/', include("web.urls")),
     path('chat/', include("chat.urls")),
-    path('',include("frontend.urls")),
-]
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+[path('',include("frontend.urls"))]
 
 # handler403 = 'user_auth.views.error_403'
